51cf1f1f7941419a0275a6d71c7aa848784b655f
Oleg <a2208021@institutmontilivi.cat>
2023-01-19 15:47:14 +0100

    Hello Edu, i'm Oleg!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@
         public static void HelloEdu()
         {
             Console.WriteLine("Hello,Edu!");
+            Console.WriteLine("Hello, i don't know who i am.");
         }
     }
     //The code was outside the internal class. asdasd
